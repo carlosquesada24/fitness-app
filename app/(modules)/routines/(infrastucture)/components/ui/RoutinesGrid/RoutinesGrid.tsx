58b03545debfed80d8fa7b4dad501481a5f89527
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-01-27 21:06:29 -0500

    RoutinesGrid component added

---
+++ app/(modules)/routines/(infrastucture)/components/ui/RoutinesGrid/RoutinesGrid.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { routinesList } from '@/app/(modules)/routines/data'
+import Link from 'next/link'
+
+const RoutinesGrid = () => {
+  return (
+    <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-12 w-full'>
+        {
+          routinesList.length > 0 ?
+          routinesList.map((routine, index) => {
+            return (
+              <Link
+                href={`/routines/${routine.id}`}
+              >
+              <article key={index} className='
+                p-4 
+                flex 
+                
+                flex-col
+                sm:flex-row
+
+                justify-between 
+                hover:cursor-pointer
+                hover:bg-[#2e2e2e]
+                transition
+                duration-350
+              '>
+                <div className='text-center sm:text-left my-3 sm:my-0'>
+                  <h3 className='md:text-[32px] text-[26px] font-bold my-2 sm:my-0'>{routine.name}</h3>
+                  <span>{routine.totalExercisesCount} exercises</span>
+                </div>
+                <div className='text-center sm:text-left my-3 sm:my-0'>
+                  <span className='block md:text-[32px] text-[26px] font-bold my-2 sm:my-0'>23%</span>
+                  <span>{routine.estimatedTime} min</span>
+                </div>
+              </article>
+              </Link>
+            )
+          })
+          : <h3>There are no routines yet</h3>
+        }
+        </div>
+  )
+}
+
+export default RoutinesGrid
