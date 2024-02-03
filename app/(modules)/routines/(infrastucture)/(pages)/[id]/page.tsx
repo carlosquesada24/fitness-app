3c5cdde38c05783470186e6ea16e16164db1cdee
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-02-03 11:12:18 -0500

    Workout logs rendered in routine view

--- app/(modules)/routines/(infrastucture)/(pages)/[id]/page.tsx
+++ app/(modules)/routines/(infrastucture)/(pages)/[id]/page.tsx
@@ -10,7 +10,7 @@ import { RoutinesContext, useRoutines } from '../../(hooks)/RoutinesContext'
 
 const Page = ({ params }: any) => {
 
-  const {routinesList} = useContext(RoutinesContext)
+  const { routinesList, sessionWorkout } = useContext(RoutinesContext)
 
   const routine = routinesList?.find(routine => routine.id === params.id)
   const exercisesListFiltered = routine?.exercisesList ?? []
@@ -26,7 +26,7 @@ const Page = ({ params }: any) => {
             ${buttonStyles}
             mr-5
             `}
-      >Start</Link>
+        >Start</Link>
 
         <span className='mr-5'>32 min</span>
 
@@ -37,6 +37,39 @@ const Page = ({ params }: any) => {
       <ExercisesGrid
         exersisesList={exercisesListFiltered}
       />
+
+      <h2 className={`${HEADINGS.H3} font-bold mt-12 mb-1`}>Workout Logs</h2>
+
+      <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-12 w-full'>
+        {
+          sessionWorkout.length > 0 ?
+            sessionWorkout.map((session: any) => {
+              return (
+                <Link
+                  key={crypto.randomUUID()}
+                  href={`/routines/${session.id}`}
+                >
+                  <article key={crypto.randomUUID()} className='
+                p-4 
+                flex 
+                
+                flex-col
+                sm:flex-row
+
+                justify-between 
+                hover:cursor-pointer
+                hover:bg-[#2e2e2e]
+                transition
+                duration-350
+              '>
+                    <h2>{session.date}</h2>
+                  </article>
+                </Link>
+              )
+            })
+            : <h3>There are no routines yet</h3>
+        }
+      </div>
     </>
   )
 }
