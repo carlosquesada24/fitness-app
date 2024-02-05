b6a72800e8f31207e09322db440a382f9855c6ac
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-02-04 19:49:07 -0500

    HandleNext and HandleFinish

--- app/(modules)/routines/(infrastucture)/(pages)/session/[id]/page.tsx
+++ app/(modules)/routines/(infrastucture)/(pages)/session/[id]/page.tsx
@@ -5,28 +5,30 @@ import { buttonStyles } from '@/app/(styles)';
 import { HEADINGS } from '@/app/(styles)/variables';
 import Link from 'next/link';
 import React, { useState } from 'react'
+import { useRoutines } from '../../../(hooks)/RoutinesContext';
+import { WorkoutSessionLog } from '@/app/(modules)/routines/domain/entities';
 
 const exercises = [
     {
-        id: 1,
+        id: "cc183164-6d73-4195-83d3-867b244a720a",
         name: "Standard bench press",
         estimatedTime: 1,
         sets: 5,
     },
     {
-        id: 2,
+        id: "2",
         name: "Incline bench press",
         estimatedTime: 1,
         sets: 7,
     },
     {
-        id: 3,
+        id: "3",
         name: "Pecs flys",
         estimatedTime: 1,
         sets: 5,
     },
     {
-        id: 4,
+        id: "4",
         name: "Pecs extensions",
         estimatedTime: 1,
         sets: 7,
@@ -45,25 +47,81 @@ interface Form {
     weightUnit: string;
 }
 
+const workoutSessionInitialState: WorkoutSessionLog = {
+    id: crypto.randomUUID(),
+    date: new Date().toISOString(),
+    exercises: [],
+}
+
+
 const Page = ({ params }: any) => {
     const [currentExercise, setCurrentExercise] = useState(exercises[0])
     const [currentSet, setCurrentSet] = useState(1)
     const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
+    const [workoutSession, setWorkoutSession] = useState(workoutSessionInitialState)
 
-    const {values: formValues, handleInputChange} = useForm(formInitialState)
+    const { addWorkoutSession } = useRoutines()
+    const { values: formValues, handleInputChange } = useForm(formInitialState)
 
     const allSets = currentExercise.sets
     const isCurrentSetLast = currentSet === allSets
     const isLastExercise = currentExerciseIndex === exercises.length - 1
     const isNextDisabled =
         !((formValues as Form).reps > 0 && (formValues as Form).weight > 0)
-    
+
     const nextExercise = exercises[currentExerciseIndex + 1]
 
     const handleNextSet = () => {
         if (currentSet < allSets) {
             setCurrentSet(currentSet + 1)
         }
+
+        const exerciseIndex = workoutSession.exercises.findIndex(
+            (exercise) => {
+                return exercise.id === currentExercise.id
+            }
+        );
+
+        const updatedExercises =
+            workoutSession.exercises.length > 0
+                ? workoutSession.exercises.map((exercise, index) => {
+                    // If this is the exercise to update, return a new object where the `sets` array is updated
+                    if (index === exerciseIndex) {
+                        return {
+                            ...exercise,
+                            sets: [
+                                ...exercise.sets,
+                                {
+                                    number: currentSet,
+                                    reps: (formValues as Form).reps,
+                                    weight: (formValues as Form).weight,
+                                    weightUnit: (formValues as Form).weightUnit,
+                                },
+                            ],
+                        };
+                    }
+
+                    // Otherwise, return the original exercise
+                    return exercise;
+                })
+                : [{
+                    id: crypto.randomUUID(),
+                    name: currentExercise.name,
+                    sets: [
+                        {
+                            number: currentSet,
+                            reps: (formValues as Form).reps,
+                            weight: (formValues as Form).weight,
+                            weightUnit: (formValues as Form).weightUnit,
+                        },
+                    ]
+                }]
+        setWorkoutSession(
+            {
+                ...workoutSession,
+                exercises: updatedExercises
+            }
+        )
     }
 
     const handleNextExercise = () => {
@@ -81,13 +139,13 @@ const Page = ({ params }: any) => {
         }
     }
 
-    const repsInputValue = 
-        (formValues as Form).reps === 0 
-            ? "" 
+    const repsInputValue =
+        (formValues as Form).reps === 0
+            ? ""
             : (formValues as Form).reps
-    const weightInputValue = 
-        (formValues as Form).weight === 0 
-            ? "" 
+    const weightInputValue =
+        (formValues as Form).weight === 0
+            ? ""
             : (formValues as Form).weight
 
 
@@ -110,6 +168,48 @@ const Page = ({ params }: any) => {
                 font-bold ${isCurrentSetLast && !isLastExercise ? "block" : "hidden"} ${buttonStyles} lg:m-auto`,
             finish: `text-[18px] font-bold ${isCurrentSetLast && isLastExercise ? "block" : "hidden"} ${buttonStyles} lg:m-auto`,
         }
+    }
+
+    const handleFinish = () => {
+
+        const test = {
+            id: crypto.randomUUID(),
+            date: "2024-02-01T14:22:30.387Z",
+            routineId: "c1024c29-cca3-48c2-87ea-408946301c44",
+            exercises: [
+                {
+                    id: crypto.randomUUID(),
+                    name: "Standard bench press",
+                    reps: 10,
+                    weight: 100,
+                    weightUnit: "kg",
+                },
+                {
+                    id: crypto.randomUUID(),
+                    name: "Incline bench press",
+                    reps: 10,
+                    weight: 100,
+                    weightUnit: "kg",
+                },
+                {
+                    id: crypto.randomUUID(),
+                    name: "Pecs flys",
+                    reps: 10,
+                    weight: 100,
+                    weightUnit: "kg",
+                },
+                {
+                    id: crypto.randomUUID(),
+                    name: "Pecs extensions",
+                    reps: 10,
+                    weight: 100,
+                    weightUnit: "kg",
+                },
+            ],
+        }
+
+
+        addWorkoutSession("1", {})
     }
 
     return (
@@ -171,6 +271,7 @@ const Page = ({ params }: any) => {
             <Link
                 href={`/`}
                 className={sessionPageStyles.buttons.finish}
+                onClick={handleFinish}
             >Finish</Link>
         </>
     )
