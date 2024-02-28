b953820fff90384b20b3de086e75a2bce8c7d8ac
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-02-28 09:43:51 -0500

    Routine card styles fixed

--- app/(modules)/routines/(infrastucture)/components/ui/RoutinesGrid/RoutinesGrid.tsx
+++ app/(modules)/routines/(infrastucture)/components/ui/RoutinesGrid/RoutinesGrid.tsx
@@ -9,12 +9,12 @@ import { COLORS } from '@/app/(styles)/variables'
 
 const RoutinesGrid = () => {
 
- const {routinesList} = useRoutines()
+  const { routinesList } = useRoutines()
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-12 w-full'>
-        {
-          routinesList.length > 0 ?
+      {
+        routinesList.length > 0 ?
           routinesList.map((routine: any) => {
             return (
               <Link
@@ -26,14 +26,14 @@ const RoutinesGrid = () => {
                 border-[#535353]
                 `}
               >
-              <article 
-                key={crypto.randomUUID()} 
-                className={`
+                <article
+                  key={crypto.randomUUID()}
+                  className={`
                   p-4 
                   flex 
                   
                   flex-col
-                  sm:flex-row
+                  sm:flex-col
 
                   bg-[#464646]
                   
@@ -45,25 +45,22 @@ const RoutinesGrid = () => {
                   transition
                   duration-350
                 `}
-                style={{
-                  border: "1px solid #535353",
-                }}
-              >
-                <div className='text-center text-[#efefef] sm:text-left my-3 sm:my-0'>
+                  style={{
+                    border: "1px solid #535353",
+                  }}
+                >
                   <h3 className='md:text-[32px] text-[26px] font-bold my-2 sm:my-0'>{routine.name}</h3>
-                  <span className='text-[#d4cee8]'>{routine.totalExercisesCount} exercises</span>
-                </div>
-                <div className='text-center sm:text-left my-3 sm:my-0'>
-                  <span className='block md:text-[32px] text-[26px] font-bold my-2 sm:my-0'>23%</span>
-                  <span className='text-[#d4cee8]'>{routine.estimatedTime} min</span>
-                </div>
-              </article>
+                  <div className='flex w-full justify-between'>
+                    <span className='text-[#d4cee8]'>{routine.totalExercisesCount} exercises</span>
+                    <span className='text-[#d4cee8] mt-auto'>{routine.estimatedTime} min</span>
+                  </div>
+                </article>
               </Link>
             )
           })
           : <h3>There are no routines yet</h3>
-        }
-        </div>
+      }
+    </div>
   )
 }
 
