0838204360d6bf4f2d7b76c1c6c54ab20eb2d2dd
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-03-01 20:35:39 -0500

    Next-video added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { withNextVideo } = require('next-video/process')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // eslint: {
@@ -7,4 +9,4 @@ const nextConfig = {
     //   },
 }
 
-module.exports = nextConfig
+module.exports = withNextVideo(nextConfig)
