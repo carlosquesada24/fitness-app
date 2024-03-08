5b082761a09a73c518ea0412df430a6950c3f152
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-03-08 11:07:12 -0500

    LogIn and SignUp pages added

---
+++ app/(modules)/authentication/(infrastucture)/(pages)/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from '../../(components)/(Form)/Form'
+
+const page = () => {
+  return (
+    <>
+      <h1>Login</h1>
+      <Form/>
+    </>
+  )
+}
+
+export default page
---
+++ app/(modules)/authentication/(infrastucture)/(pages)/sign-up/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import Form from '../../(components)/(Form)/Form';
+
+const SignUp = () =>
+  (
+    <>
+      <h1>Sign Up</h1>
+      <Form />
+    </>
+  )
+
+export default SignUp
