96aebeaf2a124fc7769e26aad6cb47b0ff2c70d7
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-03-15 16:37:41 -0400

    Coming functionalities fixed

--- app/components/LandingSections/ComingFunctionalities/ComingFunctionalities.tsx
+++ app/components/LandingSections/ComingFunctionalities/ComingFunctionalities.tsx
@@ -3,20 +3,29 @@ import { HEADINGS } from '@/app/(styles)/variables'
 
 const comingFunctionalitiesList = [
     {
-      version: 'v2',
-      versionFontColor: '#D1A8FF',
+      theme: {
+        version: 'v2',
+        bgColor: "#a0af55",
+        fontColor: "#F8F8F8"
+      },
       title: 'Build custom routines',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, unde.',
     },
     {
-      version: 'v2',
-      versionFontColor: '#D1A8FF',
+      theme: {
+        version: 'v2',
+        bgColor: "#a0af55",
+        fontColor: "#F8F8F8"
+      },
       title: 'Set your own goals',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, unde.',
     },
     {
-      version: 'v3',
-      versionFontColor: '#d9c49a',
+      theme: {
+        version: "v3",
+        bgColor: "#6e0015",
+        fontColor: "#ffe2aE",
+      },
       title: 'Track your progress',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, unde.',
     }
@@ -25,10 +34,30 @@ const comingFunctionalitiesList = [
 const ComingFunctionalitiesSection = () => {
   return (
     <section id="coming-functionalities" className="min-h-[100vh]">
-    <h1 className={`${HEADINGS.H1} mb-6 font-bold`}>Coming functionalities</h1>
+    <h1 className={`${HEADINGS.H1} mb-6 font-bold`}>Coming Functionalities</h1>
 
     <div className="grid grid-cols-2 gap-2">
-      {comingFunctionalitiesList.map((functionality, index) => (
+    <div
+        key={crypto.randomUUID()} 
+          className="   
+            p-5
+            bg-[#464646]
+            border-[#535353]
+            rounded-[6px]
+
+            hover:cursor-pointer
+            hover:bg-[#525252]
+            transition
+            duration-350"
+        >
+          <span 
+            // className={`text-[${functionality.versionFontColor}]`}
+            className={`py-1 px-3 bg-[#a0af55] font-bold rounded-[6px] mb-4 inline-block text-[#F8F8F8]`}
+           >Release on version 2.0</span>
+          <h3 className={`${HEADINGS.H3} font-bold`}>Build custom routines</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, sed.</p>
+        </div>
+
         <div
         key={crypto.randomUUID()} 
           className="   
@@ -43,13 +72,42 @@ const ComingFunctionalitiesSection = () => {
             duration-350"
         >
           <span 
-            className={`text-[${functionality.versionFontColor}]`}
-           >Coming soon on {functionality.version}!</span>
-          <h3 className={`${HEADINGS.H3} font-bold`}>{functionality.title}</h3>
-          <p>{functionality.description}</p>
+            // className={`text-[${functionality.versionFontColor}]`}
+            className={`py-1 px-3 bg-[#a0af55] font-bold rounded-[6px] mb-4 inline-block text-[#F8F8F8]`}
+           >Release on version 2.0</span>
+          <h3 className={`${HEADINGS.H3} font-bold`}>Set your own goals</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, dicta.</p>
         </div>
-      ))
-      }
+
+        <div
+        key={crypto.randomUUID()} 
+          className="   
+            p-5
+            bg-[#464646]
+            border-[#535353]
+            rounded-[6px]
+
+            hover:cursor-pointer
+            hover:bg-[#525252]
+            transition
+            duration-350"
+        >
+          <span 
+            // className={`text-[${functionality.versionFontColor}]`}
+            className={`
+              py-1 
+              px-3 
+              bg-[#ffe2aE] 
+              font-bold 
+              rounded-[6px] 
+              mb-4 
+              inline-block 
+              text-[#c33a53]
+            `}
+           >Release on version 3.0!</span>
+          <h3 className={`${HEADINGS.H3} font-bold`}>Track your progress</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, tempore!</p>
+        </div>
 
     </div>
   </section>
--- app/components/LandingSections/HowItWorks/HowItWorks.tsx
+++ app/components/LandingSections/HowItWorks/HowItWorks.tsx
@@ -1,8 +1,10 @@
+"use client"
 import React from 'react'
-import NextVideo from 'next-video'
-import { HEADINGS } from '@/app/(styles)/variables'
 
-import LogWorkoutVideo from "../../../../videos/fitness-app-add-workout-log.mp4";
+import { CldVideoPlayer } from 'next-cloudinary';
+import 'next-cloudinary/dist/cld-video-player.css';
+
+import { HEADINGS } from '@/app/(styles)/variables'
 
 const HowItWorksSection = () => {
     let windowSize = 0;
@@ -18,16 +20,31 @@ const HowItWorksSection = () => {
                 <h1 className={`${HEADINGS.H1} mb-14 font-bold`}>How it works?</h1>
 
                 <div className="w-full mb-24 flex flex-col xl:flex-row justify-between items-center">
-                    <div className="left xl:w-[50%] xl:pr-6 mb-12 xl:mb-0 text-center xl:text-left">
-                        <h2 className={`${HEADINGS.H2} mb-4 font-bold`}>Log your workouts super easy</h2>
+                    <div className="left xl:w-[50%] xl:pr-8 mb-12 xl:mb-0 text-center xl:text-left">
+                        <h2 className={`${HEADINGS.H2} mb-4 font-bold`}>Log your workouts <span className='bg-[#D1A8FF] text-[#000] py-1 px-4 rounded-[6px] font-bold whitespace-pre'>super easy</span></h2>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, repellat ea nam natus commodi ut saepe temporibus architecto, qui similique, maxime reprehenderit adipisci aliquid quas porro voluptatem pariatur aspernatur asperiores dolorum accusantium.</p>
                     </div>
 
-                    <NextVideo
-                        style={{ zIndex: "1", ...videoStyles }}
-                        className="border border-solid border-[#555555] rounded-[6px]"
-                        src={LogWorkoutVideo}
+                    <CldVideoPlayer
+                        width="1920"
+                        height="1080"
+                        src="./fitness-app/add-workout-log"
+                        className='border-solid border-4 border-[#D1A8FF]'
                     />
+                </div>
+
+                <div className="w-full mb-24 flex flex-col xl:flex-row justify-between items-center">
+                    <CldVideoPlayer
+                        width="1920"
+                        height="1080"
+                        src="./fitness-app/sign-up"
+                        className='border-solid border-4 border-[#D1A8FF]'
+                    />
+
+                    <div className="left xl:w-[50%] xl:pl-8 mt-12 mb-12 xl:mb-0 text-center xl:text-left">
+                        <h2 className={`${HEADINGS.H2} mb-4 font-bold`}>Create your account in <span className='bg-[#D1A8FF] text-[#000] py-1 px-4 rounded-[6px] font-bold whitespace-pre-wrap'>less than 30 seconds</span></h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, repellat ea nam natus commodi ut saepe temporibus architecto, qui similique, maxime reprehenderit adipisci aliquid quas porro voluptatem pariatur aspernatur asperiores dolorum accusantium.</p>
+                    </div>
                 </div>
             </section>
         )
