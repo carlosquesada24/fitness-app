689ee002fae5413d36f04acaf8144c6732cc0eaa
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-03-16 10:07:44 -0400

    Landing Sections upgraded 2

--- app/components/LandingSections/ComingFunctionalities/ComingFunctionalities.tsx
+++ app/components/LandingSections/ComingFunctionalities/ComingFunctionalities.tsx
@@ -1,119 +1,41 @@
 import React from 'react'
 import { DEFAULT_LANDING_SECTION_TITLE, HEADINGS } from '@/app/(styles)/variables'
-
-const comingFunctionalitiesList = [
-    {
-      theme: {
-        version: 'v2',
-        bgColor: "#a0af55",
-        fontColor: "#F8F8F8"
-      },
-      title: 'Build custom routines',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, unde.',
-    },
-    {
-      theme: {
-        version: 'v2',
-        bgColor: "#a0af55",
-        fontColor: "#F8F8F8"
-      },
-      title: 'Set your own goals',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, unde.',
-    },
-    {
-      theme: {
-        version: "v3",
-        bgColor: "#6e0015",
-        fontColor: "#ffe2aE",
-      },
-      title: 'Track your progress',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, unde.',
-    }
-  ];
+import { COMING_FUNCTIONALITIES_CARD_STYLES, COMING_FUNCTIONALITIES_VERSION_TEXT_STYLES } from './styles';
 
 const ComingFunctionalitiesSection = () => {
   return (
-    <section 
-      id="coming-functionalities" 
+    <section
+      id="coming-functionalities"
       className='h-auto mb-20'
     >
-    <h1 className={`${DEFAULT_LANDING_SECTION_TITLE}`}>Coming Functionalities</h1>
+      <h1 className={`${DEFAULT_LANDING_SECTION_TITLE}`}>Coming Functionalities</h1>
 
-    <div className="grid grid-cols-2 gap-2">
-    <div
-        key={crypto.randomUUID()} 
-          className="   
-            p-5
-            bg-[#464646]
-            border-[#535353]
-            rounded-[6px]
-
-            hover:cursor-pointer
-            hover:bg-[#525252]
-            transition
-            duration-350"
-        >
-          <span 
-            // className={`text-[${functionality.versionFontColor}]`}
-            className={`py-1 px-3 bg-[#a0af55] font-bold rounded-[6px] mb-4 inline-block text-[#F8F8F8]`}
-           >Release on version 2.0</span>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+        <div className={COMING_FUNCTIONALITIES_CARD_STYLES}>
+          <span
+            className={`${COMING_FUNCTIONALITIES_VERSION_TEXT_STYLES} bg-[#a0af55] text-[#F8F8F8]`}
+          >Release on version 2.0</span>
           <h3 className={`${HEADINGS.H3} font-bold`}>Build custom routines</h3>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, sed.</p>
         </div>
 
-        <div
-        key={crypto.randomUUID()} 
-          className="   
-            p-5
-            bg-[#464646]
-            border-[#535353]
-            rounded-[6px]
-
-            hover:cursor-pointer
-            hover:bg-[#525252]
-            transition
-            duration-350"
-        >
-          <span 
-            // className={`text-[${functionality.versionFontColor}]`}
-            className={`py-1 px-3 bg-[#a0af55] font-bold rounded-[6px] mb-4 inline-block text-[#F8F8F8]`}
-           >Release on version 2.0</span>
+        <div className={COMING_FUNCTIONALITIES_CARD_STYLES}>
+          <span
+            className={`${COMING_FUNCTIONALITIES_VERSION_TEXT_STYLES} bg-[#a0af55] text-[#F8F8F8]`}
+          >Release on version 2.0</span>
           <h3 className={`${HEADINGS.H3} font-bold`}>Set your own goals</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, dicta.</p>
         </div>
 
-        <div
-        key={crypto.randomUUID()} 
-          className="   
-            p-5
-            bg-[#464646]
-            border-[#535353]
-            rounded-[6px]
-
-            hover:cursor-pointer
-            hover:bg-[#525252]
-            transition
-            duration-350"
-        >
-          <span 
-            // className={`text-[${functionality.versionFontColor}]`}
-            className={`
-              py-1 
-              px-3 
-              bg-[#ffe2aE] 
-              font-bold 
-              rounded-[6px] 
-              mb-4 
-              inline-block 
-              text-[#c33a53]
-            `}
-           >Release on version 3.0</span>
+        <div className={COMING_FUNCTIONALITIES_CARD_STYLES}>
+          <span
+            className={`${COMING_FUNCTIONALITIES_VERSION_TEXT_STYLES} bg-[#ffe2aE]  text-[#c33a53]`}
+          >Release on version 3.0</span>
           <h3 className={`${HEADINGS.H3} font-bold`}>Track your progress</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, tempore!</p>
         </div>
-
-    </div>
-  </section>
+      </div>
+    </section>
   )
 }
 
--- app/components/LandingSections/HowItWorks/HowItWorks.tsx
+++ app/components/LandingSections/HowItWorks/HowItWorks.tsx
@@ -5,6 +5,7 @@ import { CldVideoPlayer } from 'next-cloudinary';
 import 'next-cloudinary/dist/cld-video-player.css';
 
 import { DEFAULT_LANDING_SECTION_TITLE, HEADINGS } from '@/app/(styles)/variables'
+import { HOW_IT_WORKS_GROUP_STYLES, VIDEO_PLAYER_STYLES } from './styles';
 
 const HowItWorksSection = () => {
     let windowSize = 0;
@@ -18,8 +19,8 @@ const HowItWorksSection = () => {
         return (
             <section className="h-auto mb-20" >
                 <h1 className={`${DEFAULT_LANDING_SECTION_TITLE}`}>How it works?</h1>
-
-                <div className="w-full mb-24 flex flex-col xl:flex-row justify-between items-center">
+                
+                <div className={`${HOW_IT_WORKS_GROUP_STYLES} mb-24`}>
                     <div className="left xl:w-[50%] xl:pr-8 mb-12 xl:mb-0 text-center xl:text-left">
                         <h2 className={`${HEADINGS.H2} mb-4 font-bold`}>Log your workouts <span className='bg-[#D1A8FF] text-[#000] py-1 px-4 rounded-[6px] font-bold whitespace-pre'>super easy</span></h2>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, repellat ea nam natus commodi ut saepe temporibus architecto, qui similique, maxime reprehenderit adipisci aliquid quas porro voluptatem pariatur aspernatur asperiores dolorum accusantium.</p>
@@ -29,16 +30,16 @@ const HowItWorksSection = () => {
                         width="1920"
                         height="1080"
                         src="./fitness-app/add-workout-log"
-                        className='border-solid border-4 border-[#D1A8FF]'
+                        className={VIDEO_PLAYER_STYLES}
                     />
                 </div>
-
-                <div className="w-full flex flex-col xl:flex-row justify-between items-center">
+                
+                <div className={`${HOW_IT_WORKS_GROUP_STYLES}`}>
                     <CldVideoPlayer
                         width="1920"
                         height="1080"
                         src="./fitness-app/sign-up"
-                        className='border-solid border-4 border-[#D1A8FF]'
+                        className={VIDEO_PLAYER_STYLES}
                     />
 
                     <div className="left xl:w-[50%] xl:pl-8 mt-12 mb-12 xl:mb-0 text-center xl:text-left">
